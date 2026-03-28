04ec440281ca0f9eb5c1a037d65ba4d389b50b23
yassin20074 <youssef.yassin99912@gmail.com>
2026-03-28 18:55:39 +0200

    update database

--- database.py
+++ database.py
@@ -1,17 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./sqlite.db"
+# بيشوف لو في رابط من ريلوي يستخدمه، لو مفيش يستخدم بتاع جهازك (Local)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if DATABASE_URL:
+    # تعديل بسيط لأن ريلوي بيبعت الرابط بـ postgres:// و SQLAlchemy عايزاه postgresql://
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+else:
+    # ده الرابط بتاع الجهاز عندي ي هيثم 
+    DATABASE_URL = "postgresql://postgres:Yassin%402007@localhost:5432/mydb"
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+ 
