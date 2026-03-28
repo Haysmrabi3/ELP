6249025fe278186807f3d8dbc7aa00f974c929d7
yassin20074 <youssef.yassin99912@gmail.com>
2026-03-28 23:13:07 +0200

    update
    Refactor database connection URL variable for clarity.

--- database.py
+++ database.py
@@ -3,16 +3,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# بيشوف لو في رابط من ريلوي يستخدمه، لو مفيش يستخدم بتاع جهازك (Local)
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    # تعديل بسيط لأن ريلوي بيبعت الرابط بـ postgres:// و SQLAlchemy عايزاه postgresql://
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if SQLALCHEMY_DATABASE_URL:
+    
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 else:
-    # ده الرابط بتاع الجهاز عندي ي هيثم 
-    DATABASE_URL = "postgresql://postgres:Yassin%402007@localhost:5432/mydb"
 
-engine = create_engine(DATABASE_URL)
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Yassin%402007@localhost:5432/mydb"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
  
